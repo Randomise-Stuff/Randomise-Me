f6c1adad9af8791e77c9619e6fdaa4b16194d0d1
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-02-01 21:21:47 +0530

    Package Version v1.0.0

---
+++ README.md
@@ -0,0 +1,59 @@
+# Randomise Me
+
+randomise-me is a NPM package which helps you in getting random stuff for your projects.
+
+# How to install this package?
+
+```sh
+npm i randomise-me
+```
+
+# Features
+
+- `generateRandomColor()` - Generates random Hex Color Codes
+- `generateDogImage()` - Gets a random Dog Image
+- `generateCatImage()` - Gets a random Cat Image
+- `generateDogFact()` - Gets a random Dog Fact
+- `generateCatFact()` - Gets a random Cat Fact
+
+# Examples
+
+.generateRandomColor()
+
+```js
+const random = require("randomise-me");
+
+console.log(random.generateRandomColor()); //returns random Hex Color Code
+```
+
+.generateDogImage()
+
+```js
+const random = require("randomise-me");
+
+console.log(random.generateDogImage()); //returns URL of a random dog image
+```
+
+.generateCatImage()
+
+```js
+const random = require("randomise-me");
+
+console.log(random.generateCatImage()); //returns URL of a random cat image
+```
+
+.generateDogFact()
+
+```js
+const random = require("randomise-me");
+
+console.log(random.generateDogFact()); //returns URL of a random dog fact
+```
+
+.generateCatFact()
+
+```js
+const random = require("randomise-me");
+
+console.log(random.generateCatFact()); //returns URL of a random cat fact
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+const axios = require("axios");
+
+function generateRandomColor() {
+  let color = "#";
+  let codes = "0123456789ABCDEF";
+
+  for (var i = 0; i < 6; i++) {
+    color += codes[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+function generateDogImage() {
+  let url = "https://some-random-api.ml/animal/dog";
+
+  axios
+    .get(url)
+    .then((res) => {
+      return res.data.image;
+    })
+    .catch((err) => {
+      return new TypeError(err);
+    });
+}
+
+function generateCatImage() {
+  let url = "https://some-random-api.ml/animal/cat";
+
+  axios
+    .get(url)
+    .then((res) => {
+      return res.data.image;
+    })
+    .catch((err) => {
+      return new TypeError(err);
+    });
+}
+
+function generateDogFact() {
+  let url = "https://some-random-api.ml/animal/dog";
+
+  axios
+    .get(url)
+    .then((res) => {
+      return res.data.fact;
+    })
+    .catch((err) => {
+      return new TypeError(err);
+    });
+}
+
+function generateCatFact() {
+  let url = "https://some-random-api.ml/animal/cat";
+
+  axios
+    .get(url)
+    .then((res) => {
+      return res.data.fact;
+    })
+    .catch((err) => {
+      return new TypeError(err);
+    });
+}
+
+module.exports = {
+  generateRandomColor,
+  generateDogImage,
+  generateCatImage,
+  generateDogFact,
+  generateCatFact,
+};
